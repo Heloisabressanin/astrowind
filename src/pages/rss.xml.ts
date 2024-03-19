9f5373aedad6445cd0d5404fb6e903a4ef82f740
Heloisabressanin <114985653+Heloisabressanin@users.noreply.github.com>
2024-03-19 15:02:29 +0100

    UpdateBuild

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,7 +1,6 @@
 import { getRssString } from '@astrojs/rss';
 
 import { SITE, METADATA, APP_BLOG } from '~/utils/config';
-import { fetchPosts } from '~/utils/blog';
 import { getPermalink } from '~/utils/permalinks';
 
 export const GET = async () => {
@@ -12,26 +11,25 @@ export const GET = async () => {
     });
   }
 
-  const posts = await fetchPosts();
 
-  const rss = await getRssString({
-    title: `${SITE.name}’s Blog`,
-    description: METADATA?.description || '',
-    site: import.meta.env.SITE,
+  // const rss = await getRssString({
+  //   title: `${SITE.name}’s Blog`,
+  //   description: METADATA?.description || '',
+  //   site: import.meta.env.SITE,
 
-    items: posts.map((post) => ({
-      link: getPermalink(post.permalink, 'post'),
-      title: post.title,
-      description: post.excerpt,
-      pubDate: post.publishDate,
-    })),
+  //   items: posts.map((post) => ({
+  //     link: getPermalink(post.permalink, 'post'),
+  //     title: post.title,
+  //     description: post.excerpt,
+  //     pubDate: post.publishDate,
+  //   })),
 
-    trailingSlash: SITE.trailingSlash,
-  });
+  //   trailingSlash: SITE.trailingSlash,
+  // });
 
-  return new Response(rss, {
-    headers: {
-      'Content-Type': 'application/xml',
-    },
-  });
+  // return new Response(rss, {
+  //   headers: {
+  //     'Content-Type': 'application/xml',
+  //   },
+  // });
 };
